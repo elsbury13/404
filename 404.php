e37c882c8e5bb32dd1b66dea24f04771fcf666c3
vagrant <vagrant@php56.(none)>
2018-05-29 11:33:14 +0000

    Some various 404 pages
    PHP / HTML5 / Javascript API

---
+++ 404.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+	<title>404 Page Not Found</title>
+</head>
+
+<body link="#E9E9E9" vlink="#E9E9E9" text="#000000" TOPMARGIN=0 LEFTMARGIN=0 MARGINWIDTH=0 MARGINHEIGHT=0 scroll="no" style="border: 0px; overflow: hidden; margin: 0pt;">
+  <table cellpadding=0 cellspacing=0 border=0 width=100% height=100%>
+  	<tr>
+    <td width=100% height=100% align=center valing=middle>
+	<h1 class="error">404 Error</h1>
+	<img src="http://www.bristolbilbao.co.uk/img/Untitled.png" width="400px" height="200px" alt="Page Not Found" border="0">
+	<h1 class="text">Ooops you have missed the net. </h1>
+			<h2><br /> The page you are looking for may be temporarily unavailable or has not been cached</h2>
+            <p class="text_under">Please try the following:</p>
+            <ul class="list_text" style="list-style-type: none;">
+                <li>Make sure you have typed the page address in the Address bar, and that it is spelled correctly</li>
+                <li>Click <a style="color:red;" href="javascript:history.go(-1)" >Back</a>, to return to the previous page</li>
+            </ul>
+	</td>
+  </tr>
+  </table><br>
+  
+</body>
+</html>
+<!-- 
+ 
+--> 
---
+++ readme.md
@@ -0,0 +1,12 @@
+# 404 Pages
+
+Some various 404 pages
+
+PHP 404
+![Screenshot](/images/404-php.png)
+
+Api call
+![Screenshot](/images/404-api.png)
+
+HTML5 Painting
+![Screenshot](/images/404-painting.png)
